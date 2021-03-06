0970e2dad56ac1ca2bc9f8315516c70c0e3fbf7b
RogerRF <rochilson@gmail.com>
2021-03-06 20:33:22 -0300

    Arranjos e Listas Unidas

--- src/bootkampkotlin/collections/ListOf.kt
+++ src/bootkampkotlin/collections/ListOf.kt
@@ -15,18 +15,27 @@ fun main(){
     val f3 = Funcionario ( "Maria", 1300.0, "CLT")
     val f4 = Funcionario ( "Lucas", 1880.0, "PJ")
 
-    val funcionarios = listOf( f1, f2, f3, f4 )
+    val funcsA = listOf( f1, f2, f3, f4 )
 
     println("SALÁRIOS MAIORES QUE 1000.00")
-    funcionarios
+    funcsA
         .filter   { it.salario > 1000 }
         .sortedBy { it.nome }
         .forEach  { println(it.toString()) }
 
     println("ORGANIZADOS POR TIPO DE CONTRATAÇÃO")
-    funcionarios
+    funcsA
         //.sortedBy { it.nome }
         .groupBy  { it.tipoContrato }
         .forEach  { println(it.toString()) }
 
+    val funcsB = listOf(f1,f2)
+    val funcsC = listOf(f3,f4)
+
+    println("FUNCIONÁRIOS - UNION DE LISTS = B + C")
+    (funcsB union funcsC).forEach  { println(it.toString()) }
+    println("FUNCIONÁRIOS - SUBTRACT DE LISTS = A - C")
+    (funcsA subtract funcsC).forEach  { println(it.toString()) }
+    println("FUNCIONÁRIOS - INTERSECT DE LISTS = B ∩ A")
+    (funcsA intersect funcsB).forEach  { println(it.toString()) }
 }
